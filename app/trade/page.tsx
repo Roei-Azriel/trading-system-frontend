93914273b5c62e2d3041b6499b3fcc0f70475a95
Roei Azriel <your-email@gmail.com>
2026-04-08 20:44:38 +0300

    start trading page

--- app/trade/page.tsx
+++ app/trade/page.tsx
@@ -1,10 +1,13 @@
-import TradingGraph from "@/components/trade/TradingGraph";
+import TradingGraph from "@/components/trade/graph/TradingGraph";
+import OrderBook from '@/components/trade/orderBook/OrderBook';
 import classes from './trade.module.css';
 
 export default function Home(){
     return(
         <div className={classes.tradeLayout}>
-            <div></div>
+            <div>
+                <OrderBook/>
+            </div>
             <div className= {classes.chartPanel}>
                 <TradingGraph/>
             </div>
